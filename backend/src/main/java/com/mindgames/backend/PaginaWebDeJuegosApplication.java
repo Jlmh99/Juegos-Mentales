c80a3e5cf02af8f0c63fffe3e2821344d635c0d3
Jlmh99 <122195336+Jlmh99@users.noreply.github.com>
2026-03-26 08:44:26 -0600

    Primer Commit: Estructura inicial

---
+++ backend/src/main/java/com/mindgames/backend/PaginaWebDeJuegosApplication.java
@@ -0,0 +1,13 @@
+package com.mindgames.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaWebDeJuegosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaWebDeJuegosApplication.class, args);
+	}
+
+}
